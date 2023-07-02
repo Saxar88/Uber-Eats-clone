1134bac803a05108ef58b86b39a2fe24db5a27ab
Saxar88 <v.sasaran@protonmail.com>
2023-07-02 08:45:48 +0300

    build Search Bar component

--- screens/Home.js
+++ screens/Home.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import { SafeAreaView } from 'react-native';
+import { SafeAreaView, View } from 'react-native';
 import SafeViewAndroid from '../components/SafeViewAndroid';
 import HeaderTabs from '../components/HeaderTabs';
+import SearchBar from '../components/SearchBar';
 
 export default function Home() {
 	return (
 		<SafeAreaView style={SafeViewAndroid.AndroidSafeArea}>
-			<HeaderTabs />
+			<View style={{ backgroundColor: 'white', padding: 15 }}>
+				<HeaderTabs />
+				<SearchBar />
+			</View>
 		</SafeAreaView>
 	);
 }
